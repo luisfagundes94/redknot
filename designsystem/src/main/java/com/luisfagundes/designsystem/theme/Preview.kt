46f161f7778c045db892e5476b9bb1fe37b7e0a7
lf <luisfelipecf94@gmail.com>
2025-11-20 20:09:47 -0300

    Refactor composable previews to use the theme's background color.

--- designsystem/src/main/java/com/luisfagundes/designsystem/theme/Preview.kt
+++ designsystem/src/main/java/com/luisfagundes/designsystem/theme/Preview.kt
@@ -1,32 +1,33 @@
 package com.luisfagundes.designsystem.theme
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun RedknotThemePreview(content: @Composable () -> Unit) {
     RedknotTheme {
-        Box {
+        Box(
+            modifier = Modifier.background(color = MaterialTheme.colorScheme.background)
+        ) {
             content.invoke()
         }
     }
 }
 
 @Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    backgroundColor = 0xFF1C110F,
-    showBackground = true,
+    uiMode = Configuration.UI_MODE_NIGHT_YES
 )
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 internal annotation class DarkPreview
 
 @Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_NO,
-    backgroundColor = 0xFFFFF8F7,
-    showBackground = true
+    uiMode = Configuration.UI_MODE_NIGHT_NO
 )
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
