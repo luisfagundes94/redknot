a5d35da1d33edc062caa40ef82c9784a9e14118a
lf <luisfelipecf94@gmail.com>
2025-12-02 18:59:08 -0300

    Reorder Compose dependencies in build.gradle.kts

--- feature/trip/build.gradle.kts
+++ feature/trip/build.gradle.kts
@@ -60,7 +60,6 @@ dependencies {
     // Hilt
     implementation(libs.hilt.android)
     implementation(libs.androidx.hilt.navigation.compose)
-    implementation(libs.androidx.compose.foundation)
     ksp(libs.hilt.compiler)
 
     // Coil
@@ -69,6 +68,7 @@ dependencies {
 
     // Compose
     implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
