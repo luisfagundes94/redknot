335ef2f189a564690f156a43f10f27d5e85cbfc6
lf <luisfelipecf94@gmail.com>
2025-12-06 12:11:50 -0300

    Remove unused capitalize function

--- feature/trip/src/main/java/com/luisfagundes/trip/tools/extensions/StringExtensions.kt
+++ feature/trip/src/main/java/com/luisfagundes/trip/tools/extensions/StringExtensions.kt
@@ -2,10 +2,6 @@ package com.luisfagundes.trip.tools.extensions
 
 import java.util.Locale
 
-fun String.capitalize(): String {
-    return replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.ROOT) else it.toString() }
-}
-
 fun String.capitalizeEveryWord(): String {
     return split(" ").joinToString(" ") { word ->
         word.replaceFirstChar {
