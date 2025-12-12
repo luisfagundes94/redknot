29ac8c7b1ba7c2c3ab0714ca72da3bea95c652fb
lf <luisfelipecf94@gmail.com>
2025-12-12 19:59:36 -0300

    Remove unused import for PreviewScreenSizes in MainActivity

--- app/src/main/java/com/luisfagundes/redknot/MainActivity.kt
+++ app/src/main/java/com/luisfagundes/redknot/MainActivity.kt
@@ -16,7 +16,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.luisfagundes.designsystem.theme.RedknotTheme
 import com.luisfagundes.redknot.navigation.AppNavDisplay
 import com.luisfagundes.redknot.navigation.Navigator
