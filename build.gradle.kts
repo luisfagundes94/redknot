3cba1e474fdb3f21f5870440b23bb406dc77bf1c
lf <luisfelipecf94@gmail.com>
2025-12-12 16:45:02 -0300

    Add detekt to project

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,15 @@ plugins {
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.secrets) apply false
+    alias(libs.plugins.detekt)
+}
+
+subprojects {
+    apply(plugin = "dev.detekt")
+
+    detekt {
+        toolVersion = "2.0.0-alpha.1"
+        config.setFrom(file("$rootDir/config/detekt/detekt.yml"))
+        buildUponDefaultConfig = true
+    }
 }
