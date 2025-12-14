b52e4e104cc669e631f1b650f8c96b2c8f70d2d4
lf <luisfelipecf94@gmail.com>
2025-12-13 21:03:20 -0300

    Add MAX_LIMIT constant to unscheduled trip status display order

--- feature/trip/src/main/java/com/luisfagundes/trip/domain/model/TripStatus.kt
+++ feature/trip/src/main/java/com/luisfagundes/trip/domain/model/TripStatus.kt
@@ -1,8 +1,10 @@
 package com.luisfagundes.trip.domain.model
 
+private const val MAX_LIMIT = 999
+
 internal enum class TripStatus(val displayOrder: Int) {
     ONGOING(0),
     UPCOMING(1),
     PAST(2),
-    UNSCHEDULED(999)
+    UNSCHEDULED(MAX_LIMIT)
 }
