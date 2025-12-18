ad2d8067f6baa76ea8f396be99bdf002450c91e4
lf <luisfelipecf94@gmail.com>
2025-12-18 15:08:25 -0300

    Remove unnecessary local date and time serializers

--- feature/trip/src/main/java/com/luisfagundes/trip/data/model/TripEntity.kt
+++ feature/trip/src/main/java/com/luisfagundes/trip/data/model/TripEntity.kt
@@ -3,15 +3,13 @@ package com.luisfagundes.trip.data.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.luisfagundes.common.data.database.converters.serializers.LocalDateSerializer
-import kotlinx.serialization.Serializable
 import java.time.LocalDate
 
 @Entity(tableName = "trips")
 internal data class TripEntity(
     @PrimaryKey(autoGenerate = true) val id: Int,
-    @ColumnInfo("start_date") @Serializable(with = LocalDateSerializer::class) val startDate: LocalDate,
-    @ColumnInfo("end_date") @Serializable(with = LocalDateSerializer::class) val endDate: LocalDate,
+    @ColumnInfo("start_date") val startDate: LocalDate,
+    @ColumnInfo("end_date") val endDate: LocalDate,
     @ColumnInfo("image_url") val imageUrl: String,
     @ColumnInfo("title") val title: String,
     @ColumnInfo("location") val location: String
