8228c58aa9b3056e64fa7ea474f8a6e900aa098f
lf <luisfelipecf94@gmail.com>
2026-04-25 12:21:43 -0300

    refactor(itinerary): change source to be itinerary item interface

--- feature/trip/src/main/java/com/luisfagundes/itinerary/presentation/mapper/ItineraryIconMapper.kt
+++ feature/trip/src/main/java/com/luisfagundes/itinerary/presentation/mapper/ItineraryIconMapper.kt
@@ -5,11 +5,16 @@ import androidx.compose.material.icons.filled.Flight
 import androidx.compose.material.icons.filled.Hotel
 import androidx.compose.material.icons.filled.LocalActivity
 import androidx.compose.material.icons.filled.Restaurant
+import com.luisfagundes.itinerary.domain.model.Accommodation
+import com.luisfagundes.itinerary.domain.model.Activity
+import com.luisfagundes.itinerary.domain.model.Flight
+import com.luisfagundes.itinerary.domain.model.ItineraryItem
 import com.luisfagundes.itinerary.domain.model.ItineraryItemType
+import com.luisfagundes.itinerary.domain.model.Restaurant
 
-internal fun ItineraryItemType.toIcon() = when (this) {
-    ItineraryItemType.ACCOMMODATION -> Icons.Default.Hotel
-    ItineraryItemType.RESTAURANT -> Icons.Default.Restaurant
-    ItineraryItemType.ACTIVITY -> Icons.Default.LocalActivity
-    ItineraryItemType.FLIGHT -> Icons.Default.Flight
+internal fun ItineraryItem.toIcon() = when (this) {
+    is Accommodation -> Icons.Default.Hotel
+    is Restaurant -> Icons.Default.Restaurant
+    is Activity -> Icons.Default.LocalActivity
+    is Flight -> Icons.Default.Flight
 }
