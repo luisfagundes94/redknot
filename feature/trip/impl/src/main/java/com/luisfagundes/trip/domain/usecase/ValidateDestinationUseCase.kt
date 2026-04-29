f564c966f46fef3b1f8cdd632a5d91fd2b6b0bff
lf <luisfelipecf94@gmail.com>
2026-04-29 17:59:34 -0300

    refactor(trip): simplify syntax in ValidateDestinationUseCase

--- feature/trip/impl/src/main/java/com/luisfagundes/trip/domain/usecase/ValidateDestinationUseCase.kt
+++ feature/trip/impl/src/main/java/com/luisfagundes/trip/domain/usecase/ValidateDestinationUseCase.kt
@@ -2,20 +2,16 @@ package com.luisfagundes.trip.domain.usecase
 
 import com.luisfagundes.common.domain.model.FieldValidationError
 import com.luisfagundes.common.domain.model.FieldValidationResult
+import com.luisfagundes.common.domain.model.FieldValidationResult.Invalid
+import com.luisfagundes.common.domain.model.FieldValidationResult.Valid
 import javax.inject.Inject
 
 internal class ValidateDestinationUseCase @Inject constructor() {
     operator fun invoke(destination: String): FieldValidationResult {
         return when {
-            destination.isBlank() -> {
-                FieldValidationResult.Invalid(FieldValidationError.EMPTY)
-            }
-
-            destination.any { it.isDigit() } -> {
-                FieldValidationResult.Invalid(FieldValidationError.CONTAINS_NUMBER)
-            }
-
-            else -> FieldValidationResult.Valid
+            destination.isBlank() -> Invalid(FieldValidationError.EMPTY)
+            destination.any { it.isDigit() } -> Invalid(FieldValidationError.CONTAINS_NUMBER)
+            else -> Valid
         }
     }
 }
