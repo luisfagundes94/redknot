a82796c82ad06ded4643b4e63838071d43c714e9
lf <luisfelipecf94@gmail.com>
2026-04-29 18:00:16 -0300

    refactor(trip): simplify FieldValidationResult usage in ValidateAddressUseCase

--- feature/trip/impl/src/main/java/com/luisfagundes/common/domain/usecase/ValidateAddressUseCase.kt
+++ feature/trip/impl/src/main/java/com/luisfagundes/common/domain/usecase/ValidateAddressUseCase.kt
@@ -3,13 +3,14 @@ package com.luisfagundes.common.domain.usecase
 import com.luisfagundes.common.domain.model.FieldValidationError
 import com.luisfagundes.common.domain.model.FieldValidationResult
 import com.luisfagundes.common.domain.model.FieldValidationResult.Invalid
+import com.luisfagundes.common.domain.model.FieldValidationResult.Valid
 import javax.inject.Inject
 
 internal class ValidateAddressUseCase @Inject constructor() {
     operator fun invoke(address: String?): FieldValidationResult {
         return when {
             address.isNullOrBlank() -> Invalid(FieldValidationError.EMPTY)
-            else -> FieldValidationResult.Valid
+            else -> Valid
         }
     }
 }
