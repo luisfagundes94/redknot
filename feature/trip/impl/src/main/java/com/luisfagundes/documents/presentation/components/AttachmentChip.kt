0082ea7518bbd3866dd30d025b54ee7d73536e53
lf <luisfelipecf94@gmail.com>
2026-05-08 18:33:26 -0300

    refactor(attachment): remove unused import

--- feature/trip/impl/src/main/java/com/luisfagundes/documents/presentation/components/AttachmentChip.kt
+++ feature/trip/impl/src/main/java/com/luisfagundes/documents/presentation/components/AttachmentChip.kt
@@ -4,14 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.InsertDriveFile
 import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.outlined.InsertDriveFile
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
