9da0bfbe6268144ac1b485251242ed5e77462bcf
lf <luisfelipecf94@gmail.com>
2026-05-22 22:11:42 -0300

    refactor(documents): remove unused import in DocumentDetailsScreen

--- feature/trip/impl/src/main/java/com/luisfagundes/documents/presentation/screen/DocumentDetailsScreen.kt
+++ feature/trip/impl/src/main/java/com/luisfagundes/documents/presentation/screen/DocumentDetailsScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
